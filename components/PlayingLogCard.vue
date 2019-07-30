0368aac08dd9deda04c714ff0f7d4e35b57c5dbb
大谷東彦 <haruhiko@weseek.co.jp>
2019-07-31 08:58:07 +0900

    - 3行省略追加

--- components/PlayingLogCard.vue
+++ components/PlayingLogCard.vue
@@ -6,7 +6,7 @@
         <h6 class="text-muted">{{ playingLog.tune.composer.displayName }}作曲</h6>
       </div>
       <b-card-text class="mb-1">{{ playingLog.user.nickname }}さんの{{ playingLog.playDate }}演奏</b-card-text>
-      <pre class="text-muted">{{ playingLog.inpression }}</pre>
+      <pre class="text-muted yrl-pre-wrap yrl-truncate-three-line">{{ playingLog.inpression }}</pre>
       <div slot="footer">
         難易度: {{ playingLog.difficulty }} 体力: {{ playingLog.physicality }} 面白さ: {{ playingLog.interesting }}
         <div>
@@ -29,3 +29,13 @@ export default class PlayingLogCard extends Vue {
   playingLog!: PlayingLog;
 }
 </script>
+<style lang="scss" scoped>
+// 3行に固定して余った分は省略
+.yrl-truncate-three-line {
+  display: -webkit-box;
+  /* autoprefixer: off */
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+}
+</style>
