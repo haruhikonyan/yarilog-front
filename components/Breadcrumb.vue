217ac7764cdff89dee72f1e2bc7b61ed01d7c667
大谷東彦 <haruhiko@weseek.co.jp>
2019-09-19 00:33:42 +0900

    - パンくずが動的に変わるように

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -8,36 +8,77 @@
 import { PropType } from 'vue';
 import { Component, Vue, Prop } from 'vue-property-decorator';
 import { PlayingLog } from '../models/PlayingLog';
-import { Tune } from '../models/Tune';
+import { Tune, PlayStyle } from '../models/Tune';
+import { Instrument } from '../models/Instrument';
+import { Composer } from '../models/Composer';
 
 @Component({})
 export default class Breadcrumb extends Vue {
   @Prop({ type: Object as PropType<PlayingLog> })
-  playingLog!: PlayingLog | undefined;
+  playingLog!: PlayingLog | null;
   @Prop({ type: Object as PropType<Tune> })
-  tune!: Tune | undefined;
+  tune!: Tune | null;
+  @Prop({ type: Object as PropType<Composer> })
+  composer!: Composer | null;
+  @Prop({ type: Object as PropType<PlayStyle> })
+  playstyle!: PlayStyle | null;
+  @Prop({ type: Object as PropType<Instrument> })
+  instrument!: Instrument | null;
 
-  items: Object[] = [];
-
-  created() {
+  get items(): Object[] {
     if (this.playingLog) {
-      this.items = this.generatePlaingLogItems(this.playingLog);
+      return this.generatePlaingLogItems(this.playingLog);
     }
     if (this.tune) {
-      this.items = this.generateTuneItems(this.tune);
+      return this.generateTuneItems(this.tune);
     }
+    const items: Object[] = [];
+    if (this.composer) {
+      items.push(this.generateComposerItems(this.composer));
+    }
+    if (this.playstyle) {
+      items.push(this.generatePlaystyleItems(this.playstyle));
+    }
+    if (this.instrument) {
+      items.push(this.generateInstrumentItems(this.instrument));
+    }
+    items.push(this.generateSearchResultItems());
+    return items.flat();
   }
-  // 作曲家->演奏形態->曲->楽器->演奏記録
+
+  private generateComposerItems(composer: Composer) {
+    return [
+      {
+        text: composer.displayName,
+        to: `/composers/${composer.id}`
+      }
+    ];
+  }
+  private generatePlaystyleItems(playstyle: PlayStyle) {
+    return [
+      {
+        text: playstyle.name,
+        to: `/tunes?playstyleId=${playstyle.id}`
+        // to: `/playstyles/${playstyle.id}`
+      }
+    ];
+  }
+  private generateInstrumentItems(instrument: Instrument) {
+    return [
+      {
+        text: instrument.shortName,
+        to: `/tunes?instrumentId=${instrument.id}`
+        // to: `/instrumens/${instrument.id}`
+      }
+    ];
+  }
+  // 演奏形態->作曲家->曲->楽器->演奏記録
   private generatePlaingLogItems(playingLog: PlayingLog): Object[] {
     const tune = playingLog.tune;
     const instrument = playingLog.instrument;
     return [
       this.generateTuneItems(tune),
-      {
-        text: instrument.shortName,
-        to: `/tunes?instrumentId=${instrument.id}`
-        // to: `/instrumens/${instrument.id}`
-      },
+      this.generateInstrumentItems(instrument),
       {
         text: `${playingLog.user.nickname}さん演奏`,
         active: true
@@ -45,23 +86,25 @@ export default class Breadcrumb extends Vue {
     ].flat();
   }
 
-  // 作曲家->演奏形態->曲
+  // 演奏形態->作曲家->曲
   private generateTuneItems(tune: Tune): Object[] {
     const composer = tune.composer;
     const playstyle = tune.playstyle;
     return [
-      {
-        text: composer.displayName,
-        to: `/composers/${composer.id}`
-      },
-      {
-        text: playstyle.name,
-        to: `/tunes?playstyleId=${playstyle.id}`
-        // to: `/playstyles/${playstyle.id}`
-      },
+      this.generatePlaystyleItems(playstyle),
+      this.generateComposerItems(composer),
       {
         text: tune.title,
         to: `/tunes/${tune.id}`
+      }
+    ].flat();
+  }
+
+  private generateSearchResultItems(): Object[] {
+    return [
+      {
+        text: '検索結果',
+        active: true
       }
     ];
   }
