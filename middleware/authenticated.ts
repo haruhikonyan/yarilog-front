af5c56d98e03233ceecab6464d2604f700c09bc4
大谷東彦 <haruhiko@weseek.co.jp>
2019-10-12 17:42:58 +0900

    - コールバックpathをセットするように

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -1,7 +1,13 @@
 import { Middleware } from '@nuxt/types';
-const authenticated: Middleware = ({ store, redirect }) => {
+// from は types になぜかいない
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const authenticated: Middleware = ({ store, redirect, from }) => {
   // ユーザが認証されていない場合
   if (!store.state.auth) {
+    // from があれば path を callbackPath にいれる
+    if (from) {
+      return redirect('/login', { callbackPath: from.path });
+    }
     return redirect('/login');
   }
 };
