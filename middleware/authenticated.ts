ee47bea05a0f6db94fbf0de1801426856150af4a
大谷東彦 <haruhiko@weseek.co.jp>
2019-10-14 12:09:43 +0900

    - 型が不十分なので一旦jsに戻す

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -1,7 +1,20 @@
-import { Middleware } from '@nuxt/types';
-// from は types になぜかいない
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const authenticated: Middleware = ({ store, redirect, from }) => {
+// import { Middleware } from '@nuxt/types';
+// // from は types になぜかいない
+// // eslint-disable-next-line @typescript-eslint/no-unused-vars
+// const authenticated: Middleware = ({ store, redirect, from }) => {
+//   // ユーザが認証されていない場合
+//   if (!store.state.auth) {
+//     // from があれば path を callbackPath にいれる
+//     if (from) {
+//       return redirect('/login', { callbackPath: from.path });
+//     }
+//     return redirect('/login');
+//   }
+// };
+
+// export default authenticated;
+
+export default function({ store, redirect, from }) {
   // ユーザが認証されていない場合
   if (!store.state.auth) {
     // from があれば path を callbackPath にいれる
@@ -10,6 +23,4 @@ const authenticated: Middleware = ({ store, redirect, from }) => {
     }
     return redirect('/login');
   }
-};
-
-export default authenticated;
+}
