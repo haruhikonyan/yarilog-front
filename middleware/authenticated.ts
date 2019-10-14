afd8e0f4677ba23e288ead9751ee38c78911d58f
大谷東彦 <haruhiko@weseek.co.jp>
2019-10-14 17:10:53 +0900

    - やむおえず any

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -1,20 +1,6 @@
 // import { Middleware } from '@nuxt/types';
-// // from は types になぜかいない
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const authenticated: Middleware = ({ store, redirect, from }) => {
-//   // ユーザが認証されていない場合
-//   if (!store.state.auth) {
-//     // from があれば path を callbackPath にいれる
-//     if (from) {
-//       return redirect('/login', { callbackPath: from.path });
-//     }
-//     return redirect('/login');
-//   }
-// };
-
-// export default authenticated;
-
-export default function({ store, redirect, from }) {
+// from は types になぜかいなく、エラー出るので一旦 any に
+const authenticated: any = ({ store, redirect, from }) => {
   // ユーザが認証されていない場合
   if (!store.state.auth) {
     // from があれば path を callbackPath にいれる
@@ -23,4 +9,6 @@ export default function({ store, redirect, from }) {
     }
     return redirect('/login');
   }
-}
+};
+
+export default authenticated;
