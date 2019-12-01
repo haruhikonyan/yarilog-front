87acea1c755ae709b0b92d5b35167d4bee0b3c7c
大谷東彦 <haruhiko@weseek.co.jp>
2019-12-01 10:31:15 +0900

    - collbackpath に fullpath を渡すように

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -5,11 +5,11 @@ const authenticated: Middleware = ({ store, redirect, route }) => {
   const auth: LoginResultObject = store.state.auth;
   // ユーザが認証されていない場合
   if (!auth) {
-    return redirect('/login', { callbackPath: route.path });
+    return redirect('/login', { callbackPath: route.fullPath });
   }
   // 最新の規約に同意していない場合
   if (!auth.consentTos) {
-    return redirect('/terms/consent', { callbackPath: route.path });
+    return redirect('/terms/consent', { callbackPath: route.fullPath });
   }
 };
 
