3d7a9ab136270b1b4d0d2ac5e5a4b3622b130caa
大谷東彦 <haruhiko@weseek.co.jp>
2019-12-02 23:50:40 +0900

    - トップもろもろ修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <section>
-    <b-jumbotron fluid class="yrl-top-title text-sm-center p-4 mt-n3">
+    <b-jumbotron fluid class="yrl-top-title text-sm-center p-4 mt-n3 mb-3">
       <h2>音楽を奏でるすべての人へ</h2>
       <h2>演奏記録の共有サイト</h2>
       <h2>みゅーぐ</h2>
+      <p class="my-3">{{ leadSentence }}</p>
       <b-button variant="primary" block class="mt-3" to="/playing-logs/new">演奏記録を書く</b-button>
     </b-jumbotron>
     <div class="container">
@@ -15,17 +16,6 @@
           :playstyles="$store.state.playstyles"
           @on-search="search($event)"
         />
-        <b-card class="mb-2" title="みゅーぐとは">
-          <pre class="yrl-pre-wrap yrl-info text-left mb-0">
-  日本国内に楽器を演奏する人は人口の10%以上いると言われています。
-  そんな楽器、そして演奏を愛する人々の演奏した記録を曲のレビュー(演奏記録)という形で残していけるWebサービスです。
-  集まった曲のレビューを他の人が見ることで、その曲を演奏して見たいと思ってくれたり、その曲をやる機会があれば演奏のための参考にしたりできると思っています。
-  また、こういった様々な人のレビューを見ることで不毛になりがちな選曲会議の助けにもなるだろうと思っています。
-  もちろん、演奏した側もいままで演奏した記録を一元管理でき、眺めて思い出に浸ると行った使い方もできると思います。
-  その記録を見てエキストラ奏者として誘って見たり。
-  使い方は無限だと思っています。
-          </pre>
-        </b-card>
         <b-card class="mb-2" title="演奏楽器から探す">
           <!-- TODO 楽器種別分け -->
           <div class="d-flex flex-wrap text-left">
@@ -58,6 +48,16 @@
           <PlayingLogCard :playing-log="playingLog" />
         </div>
       </div>
+      <b-card class="mb-2 text-center" title="みゅーぐ開発において">
+        <pre class="yrl-pre-wrap yrl-info text-left mb-0">
+日本国内で楽器を演奏する人は人口の10%、つまり1200万人以上いると言われています。
+しかし、たくさんの人が曲や演奏にかける思いを共有するサービスは未だ存在しません。「この曲の素晴らしいところはここだ」「この曲は実はこんなことを考えながら演奏していた」そんなレビューを記録に残すことで、書く側は自身の思い出整理に用いたり、あるいは読む側はこれからの選曲会議での参考にしたり、レビュアーをエキストラに誘ってみたり、使い方は無限だと思っています。
+是非、『みゅーぐ』を自由に活用してください。
+楽器と演奏を愛するみなさんの活動の一助になれば幸いです。
+
+なお、みゅーぐの開発を手伝ってくださる方を募集しています。興味のある方は問い合わせフォームもしくはメールなどでご連絡ください。
+        </pre>
+      </b-card>
       <b-card class="mb-2 text-center" title="ジャンルから探す">
         <div class="d-flex flex-wrap text-left">
           <nuxt-link
@@ -93,6 +93,8 @@ import { TuneSearchObject } from '../models/Tune';
   }
 })
 export default class Index extends Vue {
+  leadSentence =
+    '『みゅーぐ』は楽器、そして演奏を愛する人々が演奏した記録・思い出を、演奏記録という形で残していけるWebサービスです。';
   search(tuneSearchObject: TuneSearchObject) {
     const { searchWord, instrumentId, composerId, playstyleId } = tuneSearchObject;
     this.$router.push({ path: 'tunes', query: { searchWord, instrumentId, composerId, playstyleId } });
@@ -105,7 +107,7 @@ export default class Index extends Vue {
 @import 'bootstrap/scss/mixins/_breakpoints.scss';
 .yrl-info {
   @include media-breakpoint-down(xs) {
-    font-size: 70%;
+    font-size: 80%;
   }
 }
 .yrl-top-title {
